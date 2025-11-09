9ed01a6666f6701ba1a55bdc575f1fb38187088a
Ganesh Bhambarkar <ganeshbhambarkar@gmail.com>
2025-11-09 22:12:31 +0100

    fix: use R2 bucket for saving terraform state

--- terraform/vps/provider.tf
+++ terraform/vps/provider.tf
@@ -9,6 +9,30 @@ terraform {
       version = "0.16.0"
     }
   }
+
+   backend "s3" {
+    bucket                      = "homelab-terraform-state"
+    key                         = "vps/terraform.tfstate"
+    region                      = "auto" # R2 requires this to be set to "auto"
+    endpoint                    = var.s3_endpoint
+    
+    # Credentials will be passed via environment variables
+    # access_key =  # env variable AWS_ACCESS_KEY_ID (get from BW secret a6f69723-99c3-40d1-9888-b39000ca1867)
+    # secret_key =  # env variable AWS_SECRET_ACCESS_KEY (get from BW secret c9e13db6-5c78-4c62-8110-b39000ca3ea3)
+
+    # Required settings for S3-compatible (non-AWS) providers
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
+}
+
+provider "bitwarden" {
+  access_token = var.bw_access_token
+  experimental {
+    embedded_client = true
+  }
 }
 
 provider oci {
--- terraform/vps/vars.tf
+++ terraform/vps/vars.tf
@@ -1,2 +1,13 @@
 variable region { default = "ap-mumbai-1" }
 variable ssh_public_key_path { default = "~/.ssh/gateway.pub" }
+
+variable "s3_endpoint" {
+  description = "Endpoint for S3 storage. Get it from BW secret a0bb68fa-f576-4e3c-9f43-b390015ab952"
+  type        = string
+}
+
+variable "bw_access_token" {
+  description = "Access Token for BWS"
+  type        = string
+  sensitive   = true
+}
